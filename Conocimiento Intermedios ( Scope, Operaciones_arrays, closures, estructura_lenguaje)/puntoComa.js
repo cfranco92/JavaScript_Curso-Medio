bafd95268173db9792d7b69847d40127c73b697c
Cristian Franco <cfrancobedoya@gmail.com>
2018-01-11 11:36:20 -0500

    ESTRUCTURA DEL LENGUAJE
    Se discute el problema de tener que usar el punto y coma, y cuando son obligatorios.
    
    ej:
    
    ; (function saludar(){
       console.log("Hola")
    })()
    
    ; [
       "lunes",
       "martes"
    ].forEach(function(dia){
       console.log(`Día es ${dia}`)
    })

---
+++ Conocimiento Intermedios ( Scope, Operaciones_arrays, closures, estructura_lenguaje)/puntoComa.js
@@ -0,0 +1,13 @@
+
+const nombre = "Cristian"
+
+; (function saludar() {
+  console.log(`Hola ${nombre}`)
+}) ()
+
+;[
+  "EAFIT",
+  "UdeA"
+].forEach(function(universidad){
+  console.log(universidad)
+})
